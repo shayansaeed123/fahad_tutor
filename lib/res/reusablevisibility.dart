32c64ae929a1f7ce1d46c8137685fc358f1cb5ad
Shayan Saeed <115692639+shayansaeed123@users.noreply.github.com>
2024-05-03 12:16:58 +0500

    new
    new

--- lib/res/reusablevisibility.dart
+++ lib/res/reusablevisibility.dart
@@ -1,7 +1,20 @@
 
 
+import 'package:fahad_tutor/controller/color_controller.dart';
 import 'package:flutter/cupertino.dart';
 
-Widget reusableVisiblity(){
-  return 
+Widget reusableVisiblity(BuildContext context){
+  return Container(
+    width: MediaQuery.of(context).size.width * 1,
+    height: 50,
+    decoration: BoxDecoration(
+        color: colorController.btnColor,
+        borderRadius: BorderRadius.circular(10)),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        
+      ],
+    )
+  );
 }
