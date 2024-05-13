6c5a6d19711937dc438491ab7ecbd99c2e3da423
Shayan Saeed <115692639+shayansaeed123@users.noreply.github.com>
2024-05-13 16:47:06 +0500

    new
    new

--- lib/res/reusabledailog.dart
+++ lib/res/reusabledailog.dart
@@ -1,23 +1,43 @@
 
 
+import 'package:fahad_tutor/controller/color_controller.dart';
+import 'package:fahad_tutor/res/reusableText.dart';
 import 'package:flutter/material.dart';
 
-reusableprofileInfoDialog(BuildContext context, String titletxt, String contenttxt,
-    String btntxt, Function btnontap) {
+reusableprofileInfoDialog(BuildContext context, 
+// String titletxt, String contenttxt,
+    // String btntxt, Function btnontap
+    ) {
   return showDialog(
     context: context,
     builder: (context) => AlertDialog(
-      title: Text(titletxt),
-      content: Text(contenttxt),
-      actions: [
-        ElevatedButton(
-          onPressed: () {
-            btnontap();
-            Navigator.pop(context);
-          },
-          child: Text(btntxt),
-        ),
-      ],
+      title: reusableText('Tutor Basic Information',color: colorController.blackColor,fontsize: 17,fontweight: FontWeight.bold),
+      content: Column(
+        children: [
+          reusableText('Name : ',color: colorController.grayTextColor,),
+          reusableText('Contact No : ',color: colorController.grayTextColor,),
+          reusableText('Alternate No : ',color: colorController.grayTextColor,),
+          reusableText('CNIC : ',color: colorController.grayTextColor,),
+          reusableText('City : ',color: colorController.grayTextColor,),
+          reusableText('Area : ',color: colorController.grayTextColor,),
+          reusableText('Home Address : ',color: colorController.grayTextColor,),
+          reusableText('DOB : ',color: colorController.grayTextColor,),
+          reusableText('Email : ',color: colorController.grayTextColor,),
+          reusableText('Password : ',color: colorController.grayTextColor,),
+          reusableText('Religion : ',color: colorController.grayTextColor,),
+          reusableText('Last Registration : ',color: colorController.grayTextColor,),
+          reusableText('Tutor ID : ',color: colorController.grayTextColor,),
+        ],
+      ),
+      // actions: [
+      //   ElevatedButton(
+      //     onPressed: () {
+      //       btnontap();
+      //       Navigator.pop(context);
+      //     },
+      //     child: Text(btntxt),
+      //   ),
+      // ],
     ),
   );
 }
