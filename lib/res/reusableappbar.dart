5349977c98cd839ed586b7c2cf3fd6739d407c71
Shayan Saeed <bhattishayan274@gmail.com>
2024-11-26 11:49:19 +0500

    new
    new

--- lib/res/reusableappbar.dart
+++ lib/res/reusableappbar.dart
@@ -21,13 +21,6 @@ reusableappbar(BuildContext context,Color color,Function ontap,ValueListenable<O
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             reusableappimage(context, .10, .035, 'assets/images/coin_icon.png'),
-            // Image.asset(
-            // width: MediaQuery.of(context).size.width * .10,
-            // height: MediaQuery.of(context).size.height * .04,
-            // filterQuality: FilterQuality.high,
-            // fit: BoxFit.contain,
-            // 'assets/images/coin_icon.png',),
-            // Lottie.asset('assets/images/error_lottie.json'),
             AnimatedContainer(
               duration: Duration(seconds: 1),
               width: MediaQuery.of(context).size.width * .4,
@@ -48,35 +41,19 @@ reusableappbar(BuildContext context,Color color,Function ontap,ValueListenable<O
               ontap();
             },
             child: Container(
-              // margin: EdgeInsets.only(left: MediaQuery.of(context).size.width * .03),
-              // padding: EdgeInsets.only(left: MediaQuery.of(context).size.width * .03,),
                                 child: 
-                                // MySharedPrefrence().get_profile_img() 
                                 ValueListenableBuilder(valueListenable: profileimg, builder: (context, value, child) {
                                   return value != 'https://www.fahadtutors.com/fta_admin/' ? CircleAvatar(
                                   radius: MediaQuery.of(context).size.width * 0.07,
                                   backgroundColor: colorController.blackColor,
                                   backgroundImage: NetworkImage(
-                                    // MySharedPrefrence()
-                                    //     .get_profile_img()
-                                    //     .toString(),
                                     value.toString(),
                                   ),
                                 ) : reusableappimage(context, .0, .0, 'assets/images/profile.png');
                                 },)
                               ),
-            // Container(
-            //   padding: EdgeInsets.only(left: MediaQuery.of(context).size.width * .03,),
-            //   child: reusableappimage(context, .0, .0, 'assets/images/profile.png'),
-              
-            //   Image.asset(
-            //       filterQuality: FilterQuality.high,
-            //       fit: BoxFit.contain,
-            //       'assets/images/profile.png',),
-            // ),
           ),
         ),
-        // Icon(CupertinoIcons.circle_filled,color: colorController.blackColor,size: 40,),
         actions: [
           Padding(
             padding: EdgeInsets.all(MediaQuery.of(context).size.height * 0.0153),
@@ -86,14 +63,6 @@ reusableappbar(BuildContext context,Color color,Function ontap,ValueListenable<O
               },
               child: reusableappimage(context, .115, .075, 'assets/images/not_icon.png')),
           ),
-
-          // Icon(CupertinoIcons.bell_circle_fill,color: colorController.yellowColor,size: 40,),
-          // Image.asset(
-          //   width: MediaQuery.of(context).size.width * .115,
-          //   height: MediaQuery.of(context).size.height * .1,
-          //   filterQuality: FilterQuality.high,
-          //   fit: BoxFit.contain,
-          //   'assets/images/not_icon.png',),
           SizedBox(width: MediaQuery.of(context).size.width * 0.015,)
         ],
       );
