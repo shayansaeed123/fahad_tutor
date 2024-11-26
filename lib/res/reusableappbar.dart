9dbf85c0d04d75b9743cf680f162975e61627298
Shayan Saeed <bhattishayan274@gmail.com>
2024-11-26 11:49:47 +0500

    update
    update

--- lib/res/reusableappbar.dart
+++ lib/res/reusableappbar.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:fahad_tutor/controller/color_controller.dart';
 import 'package:fahad_tutor/database/my_shared.dart';
 import 'package:fahad_tutor/res/reusableText.dart';
@@ -12,58 +10,68 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 
-reusableappbar(BuildContext context,Color color,Function ontap,ValueListenable<Object?> profileimg,Function notificationtap){
+reusableappbar(BuildContext context, Color color, Function ontap,
+    ValueListenable<Object?> profileimg, Function notificationtap) {
   return AppBar(
-        elevation: 0.0,
-        foregroundColor: colorController.whiteColor,
-        backgroundColor: colorController.whiteColor,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            reusableappimage(context, .10, .035, 'assets/images/coin_icon.png'),
-            AnimatedContainer(
-              duration: Duration(seconds: 1),
-              width: MediaQuery.of(context).size.width * .4,
-            height: MediaQuery.of(context).size.height * .028,
-            decoration: BoxDecoration(
-              color: color,
-              borderRadius: BorderRadius.circular(16.0),
-            ),
-            child: Center(child: reusableText('Earn Referral Commision',fontweight: FontWeight.bold)),
-            ),
-          ],
+    elevation: 0.0,
+    foregroundColor: colorController.whiteColor,
+    backgroundColor: colorController.whiteColor,
+    title: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        reusableappimage(context, .10, .035, 'assets/images/coin_icon.png'),
+        AnimatedContainer(
+          duration: Duration(seconds: 1),
+          width: MediaQuery.of(context).size.width * .4,
+          height: MediaQuery.of(context).size.height * .028,
+          decoration: BoxDecoration(
+            color: color,
+            borderRadius: BorderRadius.circular(16.0),
+          ),
+          child: Center(
+              child: reusableText('Earn Referral Commision',
+                  fontweight: FontWeight.bold)),
         ),
-        centerTitle: true,
-        leading: Padding(
-          padding: EdgeInsets.all(MediaQuery.of(context).size.height * 0.0153),
-          child: InkWell(
-            onTap: (){
-              ontap();
+      ],
+    ),
+    centerTitle: true,
+    leading: Padding(
+      padding: EdgeInsets.all(MediaQuery.of(context).size.height * 0.0153),
+      child: InkWell(
+        onTap: () {
+          ontap();
+        },
+        child: Container(
+            child: ValueListenableBuilder(
+          valueListenable: profileimg,
+          builder: (context, value, child) {
+            return value != 'https://www.fahadtutors.com/fta_admin/'
+                ? CircleAvatar(
+                    radius: MediaQuery.of(context).size.width * 0.07,
+                    backgroundColor: colorController.blackColor,
+                    backgroundImage: NetworkImage(
+                      value.toString(),
+                    ),
+                  )
+                : reusableappimage(
+                    context, .0, .0, 'assets/images/profile.png');
+          },
+        )),
+      ),
+    ),
+    actions: [
+      Padding(
+        padding: EdgeInsets.all(MediaQuery.of(context).size.height * 0.0153),
+        child: InkWell(
+            onTap: () {
+              notificationtap();
             },
-            child: Container(
-                                child: 
-                                ValueListenableBuilder(valueListenable: profileimg, builder: (context, value, child) {
-                                  return value != 'https://www.fahadtutors.com/fta_admin/' ? CircleAvatar(
-                                  radius: MediaQuery.of(context).size.width * 0.07,
-                                  backgroundColor: colorController.blackColor,
-                                  backgroundImage: NetworkImage(
-                                    value.toString(),
-                                  ),
-                                ) : reusableappimage(context, .0, .0, 'assets/images/profile.png');
-                                },)
-                              ),
-          ),
-        ),
-        actions: [
-          Padding(
-            padding: EdgeInsets.all(MediaQuery.of(context).size.height * 0.0153),
-            child: InkWell(
-              onTap: (){
-                notificationtap();
-              },
-              child: reusableappimage(context, .115, .075, 'assets/images/not_icon.png')),
-          ),
-          SizedBox(width: MediaQuery.of(context).size.width * 0.015,)
-        ],
-      );
+            child: reusableappimage(
+                context, .115, .075, 'assets/images/not_icon.png')),
+      ),
+      SizedBox(
+        width: MediaQuery.of(context).size.width * 0.015,
+      )
+    ],
+  );
 }
